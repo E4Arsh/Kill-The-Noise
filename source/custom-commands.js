15c2b327cb24583463898790b0032a8b05bd0085
Phil <birkal@outlook.com>
2014-03-29 00:24:01 -0700

    Setting up source folder

---
+++ source/custom-commands.js
@@ -0,0 +1,13 @@
+/**
+ * Custom Commands
+ *
+ * This is bascially where most of the core custom commands goes.
+ *
+ */
+
+var customCommands = {
+
+};
+
+Object.merge(CommandParser.commands, customCommands);
+exports.customCommands = customCommands;
